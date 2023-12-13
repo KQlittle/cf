028ace2e7f625d31f603c8b423deb39fddd6dc47
KQlittle <124491693+KQlittle@users.noreply.github.com>
2023-12-13 22:12:40 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM alpine:latest
+
+# 更新 apt 软件包索引并安装 jq 和 coreutils
+RUN apk update \
+    && apk add jq coreutils
+
+# 查看已安装的软件包
+RUN apk info jq \
+    && apk info coreutils
+
+# 设置工作目录
+WORKDIR /root
+
+# 将本地环境复制到容器中
+COPY main.sh /root
+COPY ddns.sh /root
+COPY config /app
+
+RUN chmod a+x main.sh
+
+# 定义容器启动时执行的命令
+CMD ["./main.sh"]
---
+++ main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 赋予脚本和下载的文件执行权限
+chmod a+x ddns.sh
+
+# 执行 ddns.sh
+./ddns.sh
+
+
+IPnew=$(sed -n "$((x + 2)),1p" DCF.csv | awk -F, '{print $1}');
+
+# 主循环
+while true; do
+    # 使用 ping 命令检测 IP 是否可达，超时时间设置为2秒
+    if ping -c 1 -W 2 "$IPnew" &> /dev/null; then
+        echo "$(date): IP $IPnew 可正常使用...."
+    else
+        echo "$(date): IP $IPnew 不可用，将执行IP更新..."
+        # 在此处执行需要执行的脚本
+        ./ddns.sh
+    fi
+
+    # 休眠 20 分钟
+    sleep 1200
+done
