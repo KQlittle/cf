1042be1e8ca446f7171bd5eb26a46e40b97cb4f9
KQlittle <124491693+KQlittle@users.noreply.github.com>
2023-12-17 17:14:27 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,10 @@
 FROM alpine:latest
-
-# 更新 apk 软件包索引并安装 jq、coreutils 和 openssh
 RUN apk update \
-    && apk --no-cache add jq coreutils openssl bash curl
-
-# 查看已安装的软件包
-RUN apk info jq \
-    && apk info coreutils openssl bash curl
-
-# 设置工作目录
+    && apk --no-cache add jq coreutils openssl bash curl bind-tools
 WORKDIR /root
-
-# 将本地环境复制到容器中
 COPY main.sh /root
-COPY ddns /root
-
-RUN sed -i 's|#!/bin/sh|#!/bin/bash|' /root/main.sh /root/ddns
-
-RUN chmod a+x main.sh ddns
-
-# 定义容器启动时执行的命令
+COPY ddns.sh /root
+RUN sed -i 's|#!/bin/sh|#!/bin/bash|' /root/main.sh /root/ddns.sh
+RUN chmod a+x main.sh ddns.sh
 CMD ["/root/main.sh"]
 
--- main.sh
+++ main.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # 赋予脚本和下载的文件执行权限
-chmod a+x ddns
+chmod a+x ddns.sh
 
-# 执行 ddns
-./ddns >> /opt/log_file.txt
+# 执行 ddns.sh
+./ddns.sh >> /opt/ddns_log.txt
 
 # 主循环
 while true; do
     IPnew=$(sed -n "$((x + 2)),1p" DCF.csv | awk -F, '{print $1}');
     # 使用 ping 命令检测 IP 是否可达，超时时间设置为2秒
     if ping -c 1 -W 2 "$IPnew" &> /dev/null; then
-        echo -e "$(date): IP $IPnew 可正常使用...." >> /opt/log_file.txt
+        echo -e "$(date): IP $IPnew 可正常使用...." >> /opt/ddns_log.txt
     else
-        echo -e "$(date): IP $IPnew 不可用，将执行IP更新..." >> /opt/log_file.txt
+        echo -e "$(date): IP $IPnew 不可用，将执行IP更新..." >> /opt/ddns_log.txt
         # 在此处执行需要执行的脚本
-        ./ddns >> /opt/log_file.txt
+        ./ddns.sh >> /opt/ddns_log.txt
     fi
 
     # 休眠 20 分钟
